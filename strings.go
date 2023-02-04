9f7b73ee5637885c0d987afad067459d92eb1c00
Anton Ulrich <a_ulrich@boxberry.ru>
2023-02-04 20:43:35 +0300

    Added GetStrings function

---
+++ strings.go
@@ -0,0 +1,28 @@
+package testmodules
+
+import (
+	"bufio"
+	"os"
+)
+
+// GetStrings читает построчно файл и возвращает результат в виде сегмента
+func GetStrings(fileName string) ([]string, error) {
+	var lines []string
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+	err = file.Close()
+	if err != nil {
+		return nil, err
+	}
+	if scanner.Err() != nil {
+		return nil, scanner.Err()
+	}
+	return lines, nil
+}
